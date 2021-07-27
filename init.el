21ac15e3754a4918011f93ff0eeb3e6215c8a79f
BraxtonMDevelops <braxton88888@gmail.com>
2021-07-27 11:18:07 -0400

    Started adding templates and stuff for org src blocks

--- init.el
+++ init.el
@@ -252,6 +252,13 @@
       '((emacs-lisp . t)
       (python . t)))
 
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+  (setq org-confirm-babel-evaluate nil)
+
   (push '("conf-unix" . conf-unix) org-src-lang-modes))
 
 ;; Setting up projectile
