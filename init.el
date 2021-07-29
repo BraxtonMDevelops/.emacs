7ca2ebe5dfcac10a933156837fee2389eda58750
BraxtonMDevelops <braxton88888@gmail.com>
2021-07-29 08:11:53 -0400

    Started using .org to generate my init.el file. Had to fix some bugs

--- init.el
+++ init.el
@@ -1,213 +1,149 @@
-;;Allows me to write this config literatlly
-;;(org-babel-load-file (expand-file-name "config.org"  user-emacs-directory))
-
-;;Setting up emacs server
-;;(server-start)
-
-;;Setup authorization stuffs
-(setq auth-sources '("~/.authinfo"))
-
-;;Quality of Life Changes
-
-(menu-bar-mode -1)
-(scroll-bar-mode -1) ;Disables the shown scrollbar
-(tooltip-mode -1) ;Disables tooltips
-(tool-bar-mode -1) ;Disables the toolbar
-(set-fringe-mode 10) ;More fringe :D
-(defconst custom-file "/dev/null")
-
-
-;;Enable visual bell
-
-(setq visible-bell t)
-
-(defun mj/set-font-faces()
-  (message "Loading fonts!")
-  (set-face-attribute 'default nil :font "VictorMono Nerd Font Mono" :height 240 :weight 'medium)
-  (set-face-attribute 'fixed-pitch nil :font "VictorMono Nerd Font Mono" :height 240 :weight 'medium)
-  (set-face-attribute 'variable-pitch nil :font "IBM Plex Sans" :height 250 ))
-;; Below are fonts i used to use
-;;(set-face-attribute 'default nil :font "Monofur Nerd Font Mono" :height 240)
-;; FantasqueSans Mono Nerd Font
-
-;;Sets up fonts in daemon mode
-(if (daemonp)
-    (add-hook 'server-after-make-frame-hook
-	      (lambda (frame)
-		(setq doom-modeline-icon t)
-		(with-selected-frame frame
-		  (mj/set-font-faces))))
-    (mj/set-font-faces))
-
-;;Setting up package archives and stuff
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages")))
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package command-log-mode)
-(use-package swiper)
-(use-package counsel) 
-;;Makes quitting a little bit ezpzer
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(use-package all-the-icons)
 
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-previous-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-;;set up smartparens
-(use-package smartparens)
-(smartparens-global-mode t)
-(show-paren-mode 1)
-;; Sets up Doom Modeline & Doom Theme
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-  
 (use-package doom-themes)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italics t)
 (load-theme 'doom-outrun-electric t)
 
-;;Setting up which-key
-(use-package which-key
-  :init (which-key-mode)
-  :config
-  (setq which-key-idle-delay 0.25))
+(use-package doom-modeline :init (doom-modeline-mode 1))
 
-;; making line numbers nad the modeline mine.
 (column-number-mode)
 (setq display-line-numbers-type 'relative)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(add-hook 'conf-mode-hook 'display-line-numbers-mode)
-	  
-(display-time-mode 1)
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+  (add-hook 'conf-mode-hook 'display-line-numbers-mode)
 
-;;Rainbow delimiters in programming things
+    (display-time-mode 1)
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+(defun mj/set-font-faces()
+(message "Loading fonts!")
+(set-face-attribute 'default nil :font "VictorMono Nerd Font Mono" :height 240 :weight 'medium)
+(set-face-attribute 'fixed-pitch nil :font "VictorMono Nerd Font Mono" :height 240 :weight 'medium)
+(set-face-attribute 'variable-pitch nil :font "IBM Plex Sans" :height 250 ))
 
-;;make counsel the default for lots of things
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history)))
+(if (daemonp) (add-hook 'server-after-make-frame-hook (lambda (frame) (setq doom-modeline-icon t) (with-selected-frame frame (mj/setfont-faces)))) (mj/set-font-faces))
 
-;; Make things more helpful
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-  
-;; Gives more context to ivy
+(use-package centaur-tabs)
+
+(use-package swiper)
+     (use-package ivy
+        :diminish
+        :bind (("C-s" . swiper)
+               :map ivy-minibuffer-map
+               ("TAB" . ivy-alt-done)
+               ("C-l" . ivy-alt-done)
+               ("C-k" . ivy-next-line)
+               ("C-j" . ivy-previous-line)
+               :map ivy-switch-buffer-map
+               ("C-k" . ivy-previous-line)
+               ("C-l" . ivy-done)
+               ("C-d" . ivy-switch-buffer-kill)
+               :map ivy-reverse-i-search-map
+               ("C-k" . ivy-previous-line)
+               ("C-d" . ivy-previous-i-search-kill))
+      :config
+      (ivy-mode 1))
+
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
 
-;;Installing libvterm for a better terminal
-(use-package vterm)
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . counsel-minibuffer-history)))
 
-;;sets up a leader key
-(use-package general
-  
-  :config
-  (general-evil-setup)
-   (general-def
-     :states '(normal motion)
-     "SPC" nil)
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-function-variable #'helpful-variable)
+  :bind
+  ([remap-desrcibe-function] . counsel-describe-function)
+  ([remap-describe-command] . helpful-command)
+  ([remap-describe-variable] . counsel-describe-variable)
+  ([remap-describe-key] . helpful-key))
 
-   (general-create-definer mj-noleader-def
-     :states '(normal motion)
-     :keymaps 'override)
-   (general-create-definer mj/leader-key-def
-     :prefix "SPC"
-     :states '(normal motion)
-     :keymaps 'override))
-
-;; Setting Up Flycheck a bit.
 (use-package flycheck
   :init
   (global-flycheck-mode t))
 
-;;Doing Evil
+(use-package vterm)
+
+(use-package rainbow-delimiters
+  :hook(prog-mode . rainbow-delimiters-mode))
+
+(use-package smartparens)
+(smartparens-global-mode t)
+(show-paren-mode 1)
+
 (use-package evil
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
-  
+
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'message-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
+
 (use-package evil-collection
   :after evil
   :config
-  (setq evil-collection-mode-list
-        (remove 'lispy evil-collection-mode-list))
   (evil-collection-init))
-;;Hydra Ezpz repetetive keybinds setup
 
-(use-package hydra)
+(use-package which-key
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 0.25))
 
-;; More Basic Configurations for Org
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(defun mj/org-mode-setup ()
-  (variable-pitch-mode 1)
-  (auto-fill-mode 0)
-  (visual-line-mode 1)
-  (setq evil-auto-indent nil)
-  (org-indent-mode))
-  
-;; Setting up nonsense bullets
- 
-  (use-package org-superstar  
-  :after org
-  :hook (org-mode . org-superstar-mode)
-  :custom
-  (org-superstar-remove-leading-stars t)
-  (org-superstar-headline-bullets-list '("☕" "☀" "☎" "☞" "☭" "☯" "☮")))
+(use-package general)
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-idle-delay 0.1)
+  (setq company-minimum-prefix-length 1))
+
+(use-package typescript-mode
+  :mode "\\.ts\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
 
 (defun mj/org-font-setup ()
-  ;; Replace list hyphen with dot
 
   ;; Set faces for heading levels
   (dolist (face '((org-level-1 . 1.2)
@@ -233,38 +169,54 @@
   (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
   (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
 
+(defun mj/org-mode-setup()
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq-default truncate-lines t)
+  (setq evil-auto-indent nil)
+  (org-indent-mode))
 
-;;Setting other faces to be fixed-pitch so that it looks right
+(use-package org-superstar  
+:after org
+:hook (org-mode . org-superstar-mode)
+:custom
+(org-superstar-remove-leading-stars t)
+(org-superstar-headline-bullets-list '("☕" "☀" "☎" "☞" "☭" "☯" "☮")))
 
 (use-package org
   :hook (org-mode . mj/org-mode-setup)
   :config
   (setq org-ellipsis " ▾")
   (mj/org-font-setup)
-  (setq org-agenda-start-with-log-mode t)
+  (setq org-agenda-start-with-time-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
   (setq org-agenda-files
-	'("~/Org/Tasks.org")))
-
-
-;; Making org-mode smarter
-(with-eval-after-load 'org
-  (org-babel-do-load-languages
-      'org-babel-load-languages
-      '((emacs-lisp . t)
-      (python . t)))
+        '("~/Org/Tasks.org")))
 
 (require 'org-tempo)
 
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
-  (setq org-confirm-babel-evaluate nil)
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python"))
+(setq org-confirm-babel-evaluate nil)
 
-  (push '("conf-unix" . conf-unix) org-src-lang-modes))
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+   (python . t))))
 
-;; Setting up projectile
+(defun mj/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.emacs/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'mj/org-babel-tangle-config)))
+
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
@@ -273,9 +225,9 @@
   :init
   (when (file-directory-p "~/Development")
     (setq projectile-project-search-path '("~/Development")))
-  (setq projectile-switch-project-action #'projectile-dired))
+   (setq projectile-switch-project-action #'projectile-dired))
 
-;; Setting up basics of Magit
+(setq auth-sources '("~/.authinfo"))
 
 (use-package magit
   :commands (magit-status magit-get-current-branch)
@@ -284,9 +236,3 @@
 
 (use-package forge
   :after magit)
-
-(require 'org-tempo)
-(setq-default truncate-lines t)
-(setq-default global-visual-line-mode 0)
-
-(use-package centaur-tabs)
